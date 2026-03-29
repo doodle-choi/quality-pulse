f6fddb39b80ee7d174dac119aa20e380e86c766d
Seonguk Choi <tksqkac@gmail.com>
2026-03-29 19:01:12 +0900

    fix(nginx): resolve SSL configuration missing error and certbot initialization bug

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -33,11 +33,12 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+docker-compose -f docker-compose.prod.yml run --rm --entrypoint "sh" certbot -c "\
+  mkdir -p $path && \
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 \
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
+    -subj '/CN=localhost'"
 echo
 
 echo "### Starting nginx ..."
